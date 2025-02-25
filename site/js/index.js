f0dbcc9ffcd7fbe0f3e73e223bf1145aa924e40c
DANILO DE OLIVEIRA GOMES <DANILO_4803@uirapuru.br>
2025-02-24 21:50:55 -0300

    adicionado framework aula pw 24-02

--- site/js/index.js
+++ site/js/index.js
@@ -1,14 +1,24 @@
-function logar(){
+function logar(event){
+    //impede o envio normal do formulário
+    //força a chamada a passar pelo "modal"
+
+    event.preventDefault();
+
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
 
-    if(usuario == 'admin' && senha == 'admin'){   
-        alert('Login aceito')
-        location.href = './html/home.html'
+    if(usuario == 'Danilo' && senha == 'admin'){  
+        Swal.fire({
+            title: 'Login efetuado!',
+            text: 'Bem-vindo, ' + usuario + '!',
+            icon: 'success',
+            confirmButtonText: "OK"
+        }).then(() => {
+            setTimeout(() => {
+                location.href="./html/home.html";
+            }, 100);
+        });
     }else{
-    
-        alert('Erro usuario ou senha incorretos');
+      
     }
-
-
 }
